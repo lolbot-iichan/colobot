68b57b69cbc8144e097eea3cd0073a98f3a10fbb
Tomasz Kapuściński <tomaszkax86@gmail.com>
2024-12-29 18:19:08 +0100

    Updated copyright year in colobot.rc

--- desktop/colobot.rc.cmake
+++ desktop/colobot.rc.cmake
@@ -13,7 +13,7 @@ BEGIN
 			VALUE "FileDescription", "Colobot: Gold Edition - Colonize with Bots\0"
 			VALUE "ProductName", "Colobot: Gold Edition\0"
 			VALUE "CompanyName", "TerranovaTeam\0"
-			VALUE "LegalCopyright", "Copyright (c) 2012-2014 TerranovaTeam\0"
+			VALUE "LegalCopyright", "Copyright (c) 2012-2024 TerranovaTeam\0"
 			VALUE "FileVersion", "@COLOBOT_VERSION_FULL@\0"
 			VALUE "ProductVersion", "@COLOBOT_VERSION_FULL@\0"
 			END
