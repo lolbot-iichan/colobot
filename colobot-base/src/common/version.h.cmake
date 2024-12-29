8cfd245d0c1949f6e02ab7de70cfdaf67c2487a9
Tomasz Kapuściński <tomaszkax86@gmail.com>
2024-12-29 14:32:27 +0100

    Removed unnecessary static specifiers

--- colobot-base/src/common/version.h.cmake
+++ colobot-base/src/common/version.h.cmake
@@ -4,17 +4,17 @@
 
 namespace Version
 {
-    static inline constexpr bool OFFICIAL_BUILD = @CONFIG_OFFICIAL_BUILD@;
-    static inline constexpr bool DEVELOPMENT_BUILD = @CONFIG_DEVELOPMENT_BUILD@;
-    static inline constexpr bool PORTABLE_SAVES = @CONFIG_PORTABLE_SAVES@;
-    static inline constexpr bool RELATIVE_PATHS = @CONFIG_RELATIVE_PATHS@;
+    inline constexpr bool OFFICIAL_BUILD = @CONFIG_OFFICIAL_BUILD@;
+    inline constexpr bool DEVELOPMENT_BUILD = @CONFIG_DEVELOPMENT_BUILD@;
+    inline constexpr bool PORTABLE_SAVES = @CONFIG_PORTABLE_SAVES@;
+    inline constexpr bool RELATIVE_PATHS = @CONFIG_RELATIVE_PATHS@;
 
-    static inline constexpr int BUILD_NUMBER = @BUILD_NUMBER@;
+    inline constexpr int BUILD_NUMBER = @BUILD_NUMBER@;
 
-    static inline constexpr std::string_view FULL_NAME = "Colobot: Gold Edition @COLOBOT_VERSION_FULL@";
-    static inline constexpr std::string_view VERSION = "@CMAKE_PROJECT_VERSION@";
-    static inline constexpr int MAJOR = @CMAKE_PROJECT_VERSION_MAJOR@;
-    static inline constexpr int MINOR = @CMAKE_PROJECT_VERSION_MINOR@;
-    static inline constexpr int PATCH = @CMAKE_PROJECT_VERSION_PATCH@;
-    static inline constexpr std::string_view VERSION_DISPLAY = "@COLOBOT_VERSION_DISPLAY@";
+    inline constexpr std::string_view FULL_NAME = "Colobot: Gold Edition @COLOBOT_VERSION_FULL@";
+    inline constexpr std::string_view VERSION = "@CMAKE_PROJECT_VERSION@";
+    inline constexpr int MAJOR = @CMAKE_PROJECT_VERSION_MAJOR@;
+    inline constexpr int MINOR = @CMAKE_PROJECT_VERSION_MINOR@;
+    inline constexpr int PATCH = @CMAKE_PROJECT_VERSION_PATCH@;
+    inline constexpr std::string_view VERSION_DISPLAY = "@COLOBOT_VERSION_DISPLAY@";
 }
